88e404c7920d778fdd21a30e58b9870b9c8ac91f
mean <fixounet@free.fr>
2021-03-20 19:05:32 +0100

    small optimization

--- oled/ssd1306_i2c_f1.cpp
+++ oled/ssd1306_i2c_f1.cpp
@@ -1,3 +1,9 @@
+/**
+ * This is the stm32duino bluepill specific implentation
+ * @param wire
+ * @param reset
+ */
+
 #include "ssd1306_i2c_f1.h"
 #include "ssd1306_cmd.h"
 /**
@@ -26,18 +32,13 @@ void    OLED_stm32duino::sendCommand(uint8_t cmd)
  * @param cmd
  */
 const uint8_t beginHeader[]={
-    SSD1306_COMMAND,SSD1306_SET_COLUMN_ADDR,0,127,SSD1306_SET_PAGE_ADDR,0,7
+    SSD1306_COMMAND,SSD1306_SET_COLUMN_ADDR,0,127,SSD1306_SET_PAGE_ADDR,0,7,SSD1306_DATA
 };
 void    OLED_stm32duino::beginData()
 {    
     _wire.beginTransmission(SSD1306_ADDR);
     _wire.write((uint8_t *)beginHeader,sizeof(beginHeader));
     _wire.endTransmission();   
-    
-    _wire.beginTransmission(SSD1306_ADDR);
-    _wire.write(SSD1306_DATA);
-    _wire.endTransmission();    
-    
 }
 /**
  * 
