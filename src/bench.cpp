68d786c6dd464bdee3c36c1a5734b7b5a8bd6060
mean <fixounet@free.fr>
2021-03-31 07:06:04 +0200

    benchmark 2: RLE

--- src/bench.cpp
+++ src/bench.cpp
@@ -4,14 +4,32 @@
 #include "printf.h"
 #include "dso_debug.h"
 #include "screen.h"
-
+#include "assets.h"
 extern MyScreen *myScreen;
+
+// RLE : Base = 18 ms
+
+void bench2()
+{
+    
+   while(1)
+    {
+        char st[10];
+        int before=millis();
+        myScreen->clear();
+        myScreen->rleDisplay(splash_width, splash_height, 2 , 6, splash);
+        myScreen->update();
+        int after=millis();
+        Logger("Bench RLE = %d\n",after-before);
+   }
+}
 
 // emppty 28 ms
 // 4 big print =>30 ms  @ 400 khz
 // 4 big print =>18 ms  @ 800 Khz
 // 4 big print =>15 ms  @ 1000 Khz // invert 17
 //  4 big print =>FAIL  @ 2000 Khz
+
 void bench1()
 {
     int counter=0;
@@ -32,5 +50,5 @@ void bench1()
 
 void bench()
 {
-    bench1();
+    bench2();
 }
