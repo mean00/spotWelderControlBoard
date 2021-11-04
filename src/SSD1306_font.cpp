b1a94266a72f06a0f3750d093f70c272be266e7d
mean <fixounet@free.fr>
2021-11-04 07:00:37 +0100

    change font

--- src/SSD1306_font.cpp
+++ src/SSD1306_font.cpp
@@ -2,14 +2,15 @@
 //
 #include "lnArduino.h"
 #include "gfxfont.h"
-#include "assets/fonts/Fonts/FontsFree_Net_Nurom_Bold9pt7b.h"
+//#include "assets/fonts/Fonts/FontsFree_Net_Nurom_Bold9pt7b.h"
+#include "assets/fonts/Fonts/FreeSansBold9pt7b.h"
 #include "assets/fonts/Fonts/FreeSansBold20pt7b.h"
 
 const GFXfont *getFont(int index)
 {
     switch(index)
     {
-        case 0: return &FontsFree_Net_Nurom_Bold9pt7b;break;
+        case 0: return &FreeSansBold9pt7b;break;
         case 1: return &FreeSansBold20pt7b;break;
         case 2: return &FreeSansBold20pt7b;break;
         default: xAssert(0);break;
